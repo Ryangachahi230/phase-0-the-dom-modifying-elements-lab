6f196bd941243108d6bb2a62b095e420f7170f36
ryangachahic <ryangachahi230@gmail.com>
2025-06-19 07:28:14 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const main = document.querySelector('#main');
+if (main) main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Alice is the champion'; // Replace 'Alice' with your name
